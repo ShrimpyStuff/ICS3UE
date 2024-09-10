613fc68d4341cd4a5a3133f7fb1498be7a561715
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-09-10 16:46:20 +0000

    Ok math is done very poorly.

--- challenges_math.py
+++ challenges_math.py
@@ -1,14 +1,53 @@
-import cmath
+import cmath as math
+
+def egg_carton():
+    eggs = int(input("Number of Eggs:"))
+    print("Number of Cartons that can be filled:", eggs // 12)
+    print("Eggs left over:", eggs % 12)
 
 #shows complex solutions cause why not. All I need to do is add a c
-def quadratic(a, c):
+def quadratic(a=-4.9, c=11000):
         if (a == 0):
             return "no"
         b = -1*float(input("Initial Velocity (as a positive value cause the equation flips it):"))
         try:
-            x = [((-b) - cmath.sqrt((b**2)-(4*a*c)))/(2*a), ((-b) + cmath.sqrt((b**2)-(4*a*c)))/(2*a)]
+            x = [((-b) - math.sqrt((b**2)-(4*a*c)))/(2*a), ((-b) + math.sqrt((b**2)-(4*a*c)))/(2*a)]
         except:
             return "if you kept it complex then I don't even know, otherwise no real values of x"
         return f"x could be {x}"
     
-print(quadratic(-4.9, 11000))
+def geometric():
+    a = float(input("A value:"))
+    b = float(input("B value:"))
+    print("The geometric mean is:", math.sqrt(a*b))
+
+def tempFtoC():
+    c = float(input("Input a temp in celcius to convert to fahrenheit:"))
+    print((9/5)*c+32)
+
+def cylinder():
+    r = float(input("Radius:"))
+    h = float(input("Height:"))
+    v = math.pi*(r**2)*h
+    sa = (2*(v/h))+(h*2*math.pi*r)
+    print("The volume is:", v)
+    print("The surface area is:", sa)
+
+def multiples():
+    num = int(input("Number to multiply:"))
+    m = 1;
+    while (num*m <= 20):
+        print(num*m)
+        m+=1
+
+def fibonacci():
+    n = int(input("Nth Term:"))
+    a = 0
+    b = 1
+    if (n == 0):
+        return print(f"Term #{n} is:", a);
+    for i in range(n-1):
+        c=a+b
+        a=b
+        b=c
+    print(f"Term #{n} is:", b)
