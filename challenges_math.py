08f8e3527289f7d85d8dcaecb68f734449b63a35
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-09-10 02:17:56 +0000

    Revert "cmath is so cool"
    This reverts commit 7a13ed7dea8e84594c35bf8ea8604dead8b70661.

--- challenges_math.py
+++
@@ -1,9 +0,0 @@
-import cmath
-
-def quadratic(a, b, c):
-        if (a == 0):
-            return "no"
-        x = [((-b) - cmath.sqrt((b**2)-(4*a*c)))/(2*a), ((-b) + cmath.sqrt((b**2)-(4*a*c)))/(2*a)]
-        return f"x could be {x}"
-    
-print(quadratic(1, 2, 3))
