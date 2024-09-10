19755246052c90458e29a9cc4d35d437907d488b
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-09-10 03:04:18 +0000

    Just plug in the question values (b value is arbritary and I need to make it an input)

--- challenges_math.py
+++ challenges_math.py
@@ -1,9 +1,14 @@
 import cmath
 
-def quadratic(a, b, c):
+#shows complex solutions cause why not. All I need to do is add a c
+def quadratic(a, c):
         if (a == 0):
             return "no"
-        x = [((-b) - cmath.sqrt((b**2)-(4*a*c)))/(2*a), ((-b) + cmath.sqrt((b**2)-(4*a*c)))/(2*a)]
+        b = -1*float(input("Initial Velocity (as a positive value cause the equation flips it):"))
+        try:
+            x = [((-b) - cmath.sqrt((b**2)-(4*a*c)))/(2*a), ((-b) + cmath.sqrt((b**2)-(4*a*c)))/(2*a)]
+        except:
+            return "if you kept it complex then I don't even know, otherwise no real values of x"
         return f"x could be {x}"
     
-print(quadratic(1, 2, 3))
+print(quadratic(-4.9, 11000))
