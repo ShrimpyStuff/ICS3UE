5160e5cd70a2013db80a482f73ae479b84793619
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-09-25 22:40:48 +0000

    Ok a value error now

--- Math/arborist.py
+++ Math/arborist.py
@@ -1,6 +1,10 @@
 import math
-
-angle_rad = math.radians(float(input("Please input the angle of inclination in degrees: ")))
-distance = float(input("Please input the distance from the object: "))
-height = math.tan(angle_rad)*distance
-print("The height of the object is", height)
+while True:
+    try:
+        angle_rad = math.radians(float(input("Please input the angle of inclination in degrees: ")))
+        distance = float(input("Please input the distance from the object: "))
+        height = math.tan(angle_rad)*distance
+        print("The height of the object is", height)
+        break
+    except ValueError:
+        print("Not a number")
