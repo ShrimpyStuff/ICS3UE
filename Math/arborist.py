b9bdc3994b50d78b31348d3fd499cbe10a581f97
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-09-26 16:43:24 +0000

    Oopsie

--- Math/arborist.py
+++ Math/arborist.py
@@ -3,8 +3,8 @@ while True:
     try:
         angle_rad = math.radians(float(input("Please input the angle of inclination in degrees: ")))
         distance = float(input("Please input the distance from the object: "))
-        height = math.tan(angle_rad)*distance
-        print("The height of the object is", height) + 1.65
+        height = math.tan(angle_rad)*distance + 1.65
+        print("The height of the object is", height)
         break
     except ValueError:
         print("Not a number")
