545dca3386ebb4c5a408351214de8c2feaed8004
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-10-08 17:33:44 +0000

    Phone Shop should be done

--- Loops/test.py
+++ Loops/test.py
@@ -1,15 +1,29 @@
-total = 0;
-num0 = int(input("Give me a number to add to the total: "))
-total += num0
-while True:
+import re
+monthly = 0
+
+def data():
+    global monthly
     while True:
         try:
-            num1 = int(input("Give me another input: "))
+            #This text is going to take sooo long to write and I really don't want to make a list for it
+            print("We've started to offer data plans!")
+            data = ["Pay as you go ($0)", "1 GB ($10)", "2 GB ($15)", "5 GB ($40)", "20 GB ($150)"]
+            choice = input(f"Our plans are listed below:\n{"\n".join(data)}\nEnter your selection:").lower()
+            
+            if choice == "pay as you go":
+                monthly += 0
+            elif choice == "1 gb":
+                monthly += 10
+            elif choice == "2 gb":
+                monthly += 15
+            elif choice == "5 gb":
+                monthly += 40
+            elif choice == "20 gb":
+                monthly += 150
+            else:
+                raise Exception("Not a valid selection")
             break
-        except ValueError:
-            print("Not an integer")
-    total += num1
-    answer = input("Do you want to enter another number (y/n): ").lower()
-    if answer != "y":
-        break
-print(total)
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
+    
+data()
--- PhoneShop.py
+++ PhoneShop.py
@@ -1,3 +1,5 @@
+import re
+
 total = 0;
 monthly = 0;
 
@@ -8,7 +10,7 @@ def phone(package):
     global monthly
     global options
     if package not in options:
-        raise ValueError("Not a valid phone package")
+        raise Exception("Not a valid phone package")
     total += options[package]["price"]
 
 
@@ -19,7 +21,7 @@ def storageOptions():
             global monthly
             global options
             print("This plan can get additional storage options")
-            choice = input("")
+            choice = input("Would you like an additional 64GB for $200, Additional 128GB for $350?\nType 1 for 64GB, type 2 for 128GB, or type 0 or No to stick with the default amount: ").lower()
             if (choice in ["no", "0"]):
                 break
             elif (choice == "1"):
@@ -27,39 +29,119 @@ def storageOptions():
             elif (choice == "2"):
                 total += 350
             else:
-                raise ValueError("Not a valid choice")
+                raise Exception("Not a valid choice")
             break
-        except ValueError as e:
-            print(e)
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def extraDiamonds():
+    global total
     while True:
         try:
-            choice = input("Would you like extra diamonds for $200? (y or n): ").lower()
+            choice = input("Would you like extra diamonds for $200? (Y or N): ").lower()
             if choice not in ["y", "n"]:
-                raise ValueError("Not a valid choice")
+                raise Exception("Not a valid choice")
             
-            if choice != "y":
+            if choice == "y":
                 total += 200
             break
-        except ValueError as e:
-            print(e)
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def cloudDataBackup():
-    pass
+    global total
+    while True:
+        try:
+            choice = input("Would you like to add cloud data backup? Data backup costs $20 for 1 year and $35 for 2 years?\nSelect 0 or no for no backup, 1 for 1 year, or 2 for 2 years: ").lower()
+            if (choice in ["no", "0"]):
+                break
+            elif (choice == "1"):
+                total += 20
+            elif (choice == "2"):
+                total += 35
+            else:
+                raise Exception("Not a valid choice")
+            while True:
+                try:
+                    email = input("Please enter an email to register the cloud backup: ")
+                    if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+                        raise Exception("Not a valid email")
+                    break
+                except Exception as e:
+                    print(f"\033[91mError: {e}\033[0m")
+            break
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def phoneCase():
-    pass
+    global total
+    while True:
+        try:
+            choice = input("Choose a phone case from the following\nNo Case, \nRubber($20),\nCarbon Fibre($100)\nEnter Selection Here:").lower()
+            if (choice == "no case"):
+                break
+            elif (choice == "rubber"):
+                total += 20
+            elif (choice == "carbon fibre"):
+                total += 100
+            else:
+                raise Exception("Not a valid choice")
+            break
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def charger():
-    pass
+    global total
+    while True:
+        try:
+            choice = input("Would you like an extra fast battery charger for $100? (Y or N): ").lower()
+            if choice not in ["y", "n"]:
+                raise Exception("Not a valid choice")
+            
+            if choice == "y":
+                total += 100
+            break
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def insurance(package):
-    monthly += options[package]["insurance"]
+    global monthly
+    while True:
+        try:
+            choice = input(f"Would you like insurance on this phone for ${options[package]["insurance"]} per month? (Y or N): ").lower()
+            if choice == "y":
+                monthly += options[package]["insurance"]
+            elif choice != "n":
+                raise Exception("Not a valid choice")
+            break
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 def data():
-    pass
-
+    global monthly
+    while True:
+        try:
+            #This text is going to take sooo long to write and I really don't want to make a list for it
+            print("We've started to offer data plans!")
+            data = ["Pay as you go ($0)", "1 GB ($10)", "2 GB ($15)", "5 GB ($40)", "20 GB ($150)"]
+            choice = input(f"Our plans are listed below:\n{"\n".join(data)}\nEnter your selection:").lower()
+            
+            if choice == "pay as you go":
+                monthly += 0
+            elif choice == "1 gb":
+                monthly += 10
+            elif choice == "2 gb":
+                monthly += 15
+            elif choice == "5 gb":
+                monthly += 40
+            elif choice == "20 gb":
+                monthly += 150
+            else:
+                raise Exception("Not a valid selection")
+            break
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
+            
 def main():
     global total
     global monthly
@@ -68,7 +150,7 @@ def main():
     print("Welcome to the Phone Shop!")
     while True:
         try:
-            tradeIn = input("Would you like to trade in a device for $40? (y or n): ").lower()
+            tradeIn = input("Would you like to trade in a device for $40? (Y or N): ").lower()
             if tradeIn == "y":
                 total -= 40
             package = input("Choose a phone package from the following\nMcBasic($50),\nAverage Joe($150),\nRich Kid Pro($800)\nEnter Selection Here: ").lower()
@@ -81,10 +163,15 @@ def main():
                 cloudDataBackup()
             phoneCase()
             charger()
+            insurance(package)
+            data()
             
-            print(total)
+            print(f"\033[92mYour total for today is \033[1m${total}\033[0m")
+            print(f"\033[96mYour monthly cost comes to a total of \033[1m${monthly}\033[0m")
             break
-        except ValueError as e:
-            print(e)
+        except Exception as e:
+            print(f"\033[91mError: {e}\033[0m")
 
 main()
+
+# ANSI escape colours go hard
