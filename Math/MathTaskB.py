1dc657a0ae475b95a15b297d27aba2a497e6baa4
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-10-13 00:24:22 +0000

    Update

--- Math/MathTaskB.py
+++ Math/MathTaskB.py
@@ -11,7 +11,7 @@ def circle_area(radius: float):
 def cylinder_volume(radius: float, height: float = 10):
     '''A function that calculates the volume of the cylinder using the input radius and optional height'''
     try:
-        ans = math.pi * radius**2 * height
+        ans = round(math.pi * radius**2 * height, 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -19,7 +19,7 @@ def cylinder_volume(radius: float, height: float = 10):
 def sphere_volume(radius: float):
     '''A function that calculates the volume of the sphere using the input radius and optional height'''
     try:
-        ans = 4/3 * math.pi * radius**3
+        ans = round(4/3 * math.pi * radius**3, 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -27,7 +27,7 @@ def sphere_volume(radius: float):
 def cone_volume(radius: float, height: float = 10):
     '''A function that calculates the volume of the cone using the input radius and optional height'''
     try:
-        ans = math.pi * radius**2 * height / 3
+        ans = round(math.pi * radius**2 * height / 3, 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -35,7 +35,7 @@ def cone_volume(radius: float, height: float = 10):
 def circle_circumference(radius: float):
     '''A function that calculates the circumference of a circle using the input radius and optional height'''
     try:
-        ans = math.pi *radius*2
+        ans = round(math.pi *radius*2, 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -43,7 +43,7 @@ def circle_circumference(radius: float):
 def cylinder_sa(radius: float, height: float = 10):
     '''A function that calculates the surface area of a cylinder using the input radius and optional height'''
     try:
-        ans = (math.pi * radius**2 * 2) + (math.pi*2*radius*height)
+        ans = round((math.pi * radius**2 * 2) + (math.pi*2*radius*height), 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -51,7 +51,7 @@ def cylinder_sa(radius: float, height: float = 10):
 def sphere_sa(radius: float):
     '''A function that calculates the surface area of a sphere using the input radius'''
     try:
-        ans = 4 * math.pi * radius**2
+        ans = round(4 * math.pi * radius**2, 2)
         return ans
     except not KeyboardInterrupt as e:
         print("An error occured:", e)
@@ -66,8 +66,16 @@ def cone_sa(radius: float, height: float = 10):
 
 
 def main():
-    """Calls all functions"""
-    print(f"{circle_area()}", f"{circle_circumference()}")
-    print(f"{cylinder_volume()}", f"{cylinder_sa()}")
-    print(f"{sphere_volume()}", f"{sphere_sa()}")
-    print(f"{cone_volume()}", f"{cone_sa()}")
+    """Calls all functions using an input float"""
+    while True:
+        try:
+            radius = float(input("Input a radius: "))
+            print(f"The area of the circle is {circle_area(radius)}", f"the circumference is {circle_circumference(radius)}")
+            print(f"The volume of the cylinder is {cylinder_volume(radius)}", f"the surface area of the cylinder is {cylinder_sa(radius)}")
+            print(f"The volume of the sphere is {sphere_volume(radius)}", f"the surface area of the sphere is {sphere_sa(radius)}")
+            print(f"The volume of the cone is {cone_volume(radius)}", f"the surface area of the cone is {cone_sa(radius)}")
+            break
+        except:
+            print("Not a number")
+
+main()
