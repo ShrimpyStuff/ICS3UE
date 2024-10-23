e4c6f0ccc85331c8c2405e0e3b1cb0bde502a318
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-10-23 03:04:59 +0000

    Every question but 20

--- Loops/test.py
+++ Loops/test.py
@@ -1,29 +1,13 @@
-import re
-monthly = 0
+def fibonacci():
+    n = int(input("Nth Term:"))
+    a = 0
+    b = 1
+    if (n == 0):
+        return print(f"Term #0 is:", a);
+    for i in range(n-1):
+        c=a+b
+        a=b
+        b=c
+    print(f"Term #{n} is:", b)
 
-def data():
-    global monthly
-    while True:
-        try:
-            #This text is going to take sooo long to write and I really don't want to make a list for it
-            print("We've started to offer data plans!")
-            data = ["Pay as you go ($0)", "1 GB ($10)", "2 GB ($15)", "5 GB ($40)", "20 GB ($150)"]
-            choice = input(f"Our plans are listed below:\n{"\n".join(data)}\nEnter your selection:").lower()
-            
-            if choice == "pay as you go":
-                monthly += 0
-            elif choice == "1 gb":
-                monthly += 10
-            elif choice == "2 gb":
-                monthly += 15
-            elif choice == "5 gb":
-                monthly += 40
-            elif choice == "20 gb":
-                monthly += 150
-            else:
-                raise Exception("Not a valid selection")
-            break
-        except Exception as e:
-            print(f"\033[91mError: {e}\033[0m")
-    
-data()
+fibonacci()
---
+++ Strings/strings_assignments.py
@@ -0,0 +1,23 @@
+#Question 20
+prop_n0 = input()
+noun = input()
+adj_0 = input()
+adj_1 = input()
+prop_n0 = input()
+adj_2= input()
+adj_3= input()
+
+#Question 21
+string = input("Enter a string: ")
+print(string[-1] + string[1:-1] + string[0])
+
+#Question 22
+string = input("Enter a string: ")
+print(string[1:] + string[0]+"ay")
+
+#Question 23
+name = input("Enter your name:")
+print(name + ", " + name + ", bo-b" + name[1:])
+print("Banana-fana fo-f" + name[1:])
+print("Fee-fi-mo-m" + name[1:])
+print(name + "!")
