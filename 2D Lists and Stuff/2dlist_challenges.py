f0dc1291aa36ef435b8d1f503ad29af54f4f22e6
Sajid Monowar <52041627+ShrimpyStuff@users.noreply.github.com>
2024-11-27 19:48:22 +0000

    Ok

--- 2D Lists and Stuff/2dlist_challenges.py
+++ 2D Lists and Stuff/2dlist_challenges.py
@@ -62,6 +62,23 @@ while True:
         print("Not an integer")
 
 #Question 5
-sales = {"John":{"N":3056, "S":8463, "E": 8441, "W": 2694},"Tom": {"N": 4832, "S": 6786, "E":4737, "W": 3612}, "Anne":{"N":5239, "S":4802, "E": 5820, "W": 1859},"Fiona":{"N":3904, "S":3645, "E":8821, "W": 2451]}
+sales = {"John":{"N":3056, "S":8463, "E": 8441, "W": 2694},"Tom": {"N": 4832, "S": 6786, "E":4737, "W": 3612}, "Anne":{"N":5239, "S":4802, "E": 5820, "W": 1859},"Fiona":{"N":3904, "S":3645, "E":8821, "W": 2451}}
 
 #Question 6
+while True:
+    try:
+        name = input("Give a name: ")
+        region = input("Give a region: ")
+        print("Current: " + sales[name][region])
+        while True:
+            try:
+                sales[name][region] = int(input("What is the new sales data for this region: "))
+                break
+            except ValueError:
+                print("Not a valid integer")
+        print(sales[name])
+        break
+    except KeyError:
+        print("Not a valid name or region. Maybe check your capitalization")
+
+#Question 7
